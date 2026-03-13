133c3986817b5a0c35e0e8661da7da1893ad1a3a
Anastasia Ayres <z5692718@ad.unsw.edu.au>
2026-03-14 03:52:42 +1100

    Added function to get time related to pomodoro

--- backend/src/service/auth.ts
+++ backend/src/service/auth.ts
@@ -3,3 +3,22 @@ import bcrypt from 'bcryptjs';
 import { Database } from 'sqlite';
 import { nanoid } from 'nanoid';
 
+// time in pomodoroTimer
+async function getTotalTime(userId: string, db: Database) {
+  const time = await db.get(`
+    SELECT 
+      SUM(strftime('%s', end_time) - strftime('%s', start_time)) AS total_seconds,
+      SUM(pomodoro_complete) * 25 AS total_mins_studied,
+      COUNT(*) AS total_sessions
+    FROM pomodoro_session WHERE user_id = ? AND end_time IS NOT NULL
+  `, [userId]);
+
+  // returns OBJ 
+  /**
+   * {
+   *    total_seconds
+   *    total_min_studied
+   *    total_sessions
+   * } 
+   */
+}
