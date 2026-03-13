760ad7941c13bbec73fb5f32b7d1d6e8a176fbb3
Anastasia Ayres <z5692718@ad.unsw.edu.au>
2026-03-13 23:05:44 +1100

    Fix to users db

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -17,23 +17,24 @@ export async function initDatabase(): Promise<Database<sqlite3.Database, sqlite3
 
     await db.exec(`
         CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY,
-            username TEXT NOT NULL,
-            password TEXT NOT NULL,
-            romance_character_id INTEGER,
+            id STRING PRIMARY KEY,
+            username VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            romance_character_id STRING,
             total_study TIME DEFAULT 0,
             FOREIGN KEY (romance_character_id) references chars(id)
         );
 
         CREATE TABLE IF NOT EXISTS chars (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            description TEXT
+            id STRING PRIMARY KEY,
+            name VARCHAR(255),
+            description VARCHAR(255)
         );
 
         CREATE TABLE IF NOT EXISTS  relationship (
-            user_id INTEGER,
-            char_id INTEGER,
+            user_id STRING,
+            char_id STRING,
             points INTEGER,
             affection_lvl INTEGER,
             PRIMARY KEY(user_id, char_id),
@@ -42,24 +43,24 @@ export async function initDatabase(): Promise<Database<sqlite3.Database, sqlite3
         );
 
         CREATE TABLE IF NOT EXISTS  pomodoro_session (
-            id INTEGER PRIMARY KEY,
-            user_id INTEGER,
+            id STRING PRIMARY KEY,
+            user_id STRING,
             start_time TIME,
             end_time TIME,
             pomodoro_complete INTEGER, -- default 0
-            state TEXT, -- work, break, long break - deafult 25, 5, 15 - complete
+            state VARCHAR(255), -- work, break, long break - deafult 25, 5, 15 - complete
             FOREIGN KEY (user_id) references users(id)
         );
 
         CREATE TABLE IF NOT EXISTS  timers (
-            timer_id INTEGER PRIMARY KEY, -- i think it is idk
-            user_id INTEGER,
+            timer_id STRING PRIMARY KEY, -- i think it is idk
+            user_id STRING,
             FOREIGN KEY (user_id) references users(id)
         );
     `)
 
     await db.exec(`
-        INSERT INTO chars (id, name, description) VALUES (1, 'apple', 'placeholder')
+        INSERT INTO chars (id, name, description) VALUES ('place_id', 'apple', 'placeholder')
     `)
 
     return db;
