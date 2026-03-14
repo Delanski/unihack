434caec89f50d08ef75115125d3a1d0f6bf61402
Anabel Phinu <your-email@example.com>
2026-03-15 02:50:14 +1100

    Add buttons and custom cursor

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="app">
+
+      <div className="sidebar">
+        <button className="navButton">Study</button>
+        <button className="navButton">To-Do</button>
+        <button className="navButton">Stats</button>
+        <button className="navButton">Settings</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
